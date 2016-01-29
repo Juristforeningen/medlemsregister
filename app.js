15592d60243f61318c34c504389e8f7713de8ba9
Anders Olsen Sandvik <Andersos@users.noreply.github.com>
2016-01-29 22:10:22 +0100

    Add koa-views and handlebars template

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 var koa = require('koa');
 var app = module.exports = koa();
+var views = require('koa-views');
+
+app.use(views(__dirname + '/views', {
+  map: {
+    html: 'handlebars'
+  }
+}));
 
 app.use(function *() {
-  this.body = 'Hello Juristforeningen';
+  yield this.render('template', {
+    content: 'Hello '
+  });
 });
 
 if (!module.parent) app.listen(process.env.PORT || 3000);
--- test/test.js
+++ test/test.js
@@ -1,11 +1,12 @@
+// var assert = require('assert');
 var app = require('../app.js');
 var request = require('supertest').agent(app.listen());
 
-describe('Hello Juristforeningen', function () {
-  it('should say "Hello Juristforeningen"', function (done) {
+describe('App', function () {
+  it('index should contain Juristforeningen', function (done) {
     request
     .get('/')
     .expect(200)
-    .expect('Hello Juristforeningen', done);
+    .expect(/Juristforeningen/, done);
   });
 });
