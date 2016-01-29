bde36d56bc54957738a331fa2655424e82c3d67c
Anders Olsen Sandvik <Andersos@users.noreply.github.com>
2016-01-29 13:31:04 +0100

    Change hello msg

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var koa = require('koa');
 var app = module.exports = koa();
 
 app.use(function *(){
-  this.body = 'Hello World';
+  this.body = 'Hello Juristforeningen';
 });
 
 if (!module.parent) app.listen(process.env.PORT || 3000);
